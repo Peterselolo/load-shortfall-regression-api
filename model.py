bd42f39e1dc7f908c470617a361a5a97aae436ab
peter selolo <phillemonpeterbuti@gmail.com>
2022-05-26 14:58:12 +0200

    Update model.py

--- model.py
+++ model.py
@@ -58,7 +58,7 @@ def _preprocess_data(data):
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
-    predict_vector = feature_vector_df[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+    predict_vector = feature_vector_df[['Madrid_wind_speed', 'Bilbao_pressure' , 'Valencia_temp_min' , 'Seville_temp_max','Madrid_humidity']]
     # ------------------------------------------------------------------------
 
     return predict_vector
